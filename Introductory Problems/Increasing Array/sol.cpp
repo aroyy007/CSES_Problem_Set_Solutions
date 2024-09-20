e5d5a87861fd73306995859c57db3b9dc1aef24d
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-09-20 20:43:40 +0600

    Create sol.cpp

---
+++ Introductory Problems/Increasing Array/sol.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <map>
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+ 
+int sum_d(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+ 
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+ 
+int main()
+{
+    optimize();
+ 
+    ll n;
+    cin >> n;
+ 
+    vector<ll> a(n);
+    ain(a, n);
+ 
+    ll s = 0;
+ 
+    for (ll i = 1; i < n; i++)
+    {
+        if (a[i - 1] > a[i])
+        {
+            s += (a[i - 1] - a[i]);
+            a[i] = a[i - 1];
+        }
+    }
+ 
+    cout << s << endl;
+ 
+    return 0;
+}
